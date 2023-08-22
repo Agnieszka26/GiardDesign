c9c36324b1953e82c0767188de5a0a0316b7f330
Agnieszka26 <agna.medrek@gmail.com>
2023-08-22 22:05:20 +0200

    add animations

---
+++ js/animation.js
@@ -0,0 +1,14 @@
+ScrollReveal().reveal('.revealLeft', {
+    distance: '30px',    
+    origin: 'left',     
+    delay: 200,           
+    duration: 1000,       
+    easing: 'ease-in-out',
+  });
+  ScrollReveal().reveal('.revealRight', {
+    distance: '30px',    
+    origin: 'right',   
+    delay: 200,          
+    duration: 1000,      
+    easing: 'ease-in-out', 
+  });
